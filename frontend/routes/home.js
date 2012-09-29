4e3fdbfe04f6ed4d1951950c5b31f21d3f1339dd
Justin Beckwith <justin.beckwith@gmail.com>
2012-09-29 15:56:22 -0700

    added click on image to get current details

--- frontend/routes/home.js
+++ frontend/routes/home.js
@@ -1,3 +1,4 @@
+var request = require('request');
 
 module.exports = function (app, nconf, serviceBusService) {
 
@@ -10,5 +11,15 @@ module.exports = function (app, nconf, serviceBusService) {
     app.get('/:city', function (req, res) {
         console.log(req.params.city);
         res.render('images.html');
+    });
+
+    // service calls to get media details
+    app.get('/media/:id', function (req, res) {
+        console.log('request for media: ' + req.params.id);
+        var url = "https://api.instagram.com/v1/media/" + req.params.id + "/?client_id=" + nconf.get('instagramClientId');
+        console.log(url);
+        request(url, function (e, r, b) {
+            res.end(b);            
+        });
     });
 }
