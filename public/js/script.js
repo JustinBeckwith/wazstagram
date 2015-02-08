7299c54c999987bc85ec9796813a175059d9d762
Justin Beckwith <justbe@microsoft.com>
2015-02-07 22:22:06 -0800

    clean up dependencies

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,10 @@
 var picBuffer = [];
 var picCount = 0;
 
+window.appInsights={queue:[],applicationInsightsId:null,accountId:null,appUserId:null,configUrl:null,start:function(n){function u(n,t){n[t]=function(){var i=arguments;n.queue.push(function(){n[t].apply(n,i)})}}function f(n){var t=document.createElement("script");return t.type="text/javascript",t.src=n,t.async=!0,t}function r(){i.appendChild(f("//az416426.vo.msecnd.net/scripts/ai.0.js"))}var i,t;this.applicationInsightsId=n;u(this,"logEvent");u(this,"logPageView");i=document.getElementsByTagName("script")[0].parentNode;this.configUrl===null?r():(t=f(this.configUrl),t.onload=r,t.onerror=r,i.appendChild(t));this.start=function(){}}};
+appInsights.start("2528f039-82a4-414a-8644-2931b2ea8682");
+appInsights.logPageView();
+
 $(function () {
 
     var socket = io.connect();
